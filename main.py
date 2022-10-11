eb89198125f78b12366bcdf747e14dd2ef4e4b4e
Dongwhee-1 <kevin98124@gmail.com>
2022-10-12 05:01:38 +0900

    Aha

---
+++ main.py
@@ -0,0 +1,14 @@
+N = 5
+
+from RPSLS_game import RPSLS_game
+
+from P00000 import P00000
+from P19892 import P19892
+from P99999 import P99999
+
+game = RPSLS_game(P99999, P19892)
+for i in range(1, N+1):
+    print(f"[Round {i}]")
+    game.proceed_match()
+
+print(game.get_score())
